b38a694b607a7d389adfd9212b74ab1cee226ce6
aryumka <yooooar@gmail.com>
2023-07-15 00:08:09 +0900

    섹션4

--- src/main/java/com/example/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/com/example/jpashop/repository/order/query/OrderQueryRepository.java
@@ -63,4 +63,15 @@ public class OrderQueryRepository {
 
         return result;
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new com.example.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
